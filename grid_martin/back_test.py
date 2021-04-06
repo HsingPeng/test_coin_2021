65e4bc9eb7cadce3167b8a194139d4e1aa4915bc
HsingPeng <bboxhe@gmail.com>
2021-04-07 02:16:49 +0800

    重构马丁逻辑

--- grid_martin/back_test.py
+++ grid_martin/back_test.py
@@ -19,6 +19,8 @@
 import pandas
 import matplotlib.pyplot as plt
 import json
+import time
+from multiprocessing.pool import Pool
 
 
 def show(df_1):
@@ -55,19 +57,22 @@ plt.title('BTC定投回测')
 plt.rcParams["font.family"] = 'Arial Unicode MS'
 plt.ion()
 
+def calculate_one(param):
+    pass
+
 # 初始配置
 symbol_config = {
-    'max_order_level': 4,  # 资金最多能下单几次，10代表剩余资金可以下10次单
-    'max_diff_rate': 0.12,   # 最大破网百分比
-    'total_level': 32,        # 资金分成多少份
-    # 'level_list': [1, 2, 4, 8, 16, 32, 64, 128, 256, 1024, 2048, 4096],
-    'level_list': [1, 4, 8, 16, 32],
+    'max_order_level': 5,  # 资金最多能下单几次，10代表剩余资金可以下10次单
+    'max_diff_rate': 0.2,   # 最大破网百分比
+    'total_level': 32,       # 资金分成多少份
+    # 'level_list': [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048],
+    'level_list': [1, 2, 4, 8, 16],
     # 'level_list': [1, 1, 2, 2, 4, 4, 8, 8, 16, 16, 32, 32],
     'fee': 0.001            # 手续费，千分之一
 }
 
 init_info = {
-    'init_balance_amount': 8192,        # 初始余额
+    'init_balance_amount': 200,        # 初始余额
 }
 
 # 初始化数据
@@ -95,34 +100,37 @@ show_num = 0
 df = pandas.DataFrame()
 for candle_begin_time, row in source_df.iterrows():
     series = pandas.Series({
-            'candle_begin_time': candle_begin_time,
-            'open': row['open'],
-            'high': row['high'],
-            'low': row['low'],
-            'close': row['close'],
-            'volume': row['volume'],
-            'balance_amount': info['balance_amount'],
-            'balance_size': info['balance_size'],
-            'order_num': info['order_num']
-        }, name=candle_begin_time)
+        'candle_begin_time': candle_begin_time,
+        'open': row['open'],
+        'high': row['high'],
+        'low': row['low'],
+        'close': row['close'],
+        'volume': row['volume'],
+        'balance_amount': info['balance_amount'],
+        'balance_size': info['balance_size'],
+        'order_num': info['order_num']
+    }, name=candle_begin_time)
 
     df = df.append(series, ignore_index=False)
     df = df.iloc[-max_len:]  # 保持最大K线数量不会超过max_len个
     # print(df.iloc[-1].to_json())
     # print(json.dumps(info))
     df1 = df.iloc[-1]
-    """
-    print([
-        df1['candle_begin_time'],
-        df1['close'],
-        df1['balance_amount'],
-        df1['balance_size'],
-        df1['order_num']
-    ], "\t")
-    """
-    if show_num % 20 == 0:
-        show(df)
+    if show_num % 1 == 0:
+        # show(df)
+        print([
+            df1['candle_begin_time'],
+            df1['close'],
+            df1['balance_amount'],
+            df1['balance_size'],
+            df1['order_num'],
+            json.dumps(info),
+        ], "\t")
     show_num += 1
+
+    # 方便debug
+    # if str(df1['candle_begin_time']) == '2017-09-04 05:15:00':
+    #    print('aaaa')
 
     total_level = symbol_config['total_level']
     level_list = symbol_config['level_list']
@@ -133,7 +141,7 @@ for candle_begin_time, row in source_df.iterrows():
     """
     1 先看单子能否成交
     2 填充字段
-    info : 
+    info :
     'balance_amount': 100,          # 余额
     'balance_size': 0,           # 剩余的币数
     'diff_price': 344,            # 当前价差
@@ -142,11 +150,12 @@ for candle_begin_time, row in source_df.iterrows():
     'last_buy_price': 54341,            # 上个买单的价格
     'last_buy_amount':2,                # 上个买单的数量
     'buy_order': {
-            'order_size':0.1                # 下单卖出的数量，卖单需要
-            'order_price':414134            # 下单价格
-            'order_amount':200              # 下单买入的金额，买单需要
-            'state':1                       # 状态 1 下单 2 成交
-        },
+        'order_size':0.1                # 下单卖出的数量，卖单需要
+        'order_price':414134            # 下单价格
+        'order_amount':200              # 下单买入的金额，买单需要
+        'state':'submit',               # 状态 submit 下单 finish 成交
+        'flag':'deal_now',             # 回测特殊标记，deal_now 第一个买单和最后一个爆单，直接成交
+    },
     'sell_order':{}
     """
     low = df.iloc[-1]['low']
@@ -155,27 +164,27 @@ for candle_begin_time, row in source_df.iterrows():
     sell_order = info['sell_order']
 
     # 买单成交，第一单简化，直接成交
-    if buy_order is not None:
-        if low <= buy_order['order_price'] or 1 == info['order_num']:
-            order_price = buy_order['order_price']
-            order_amount = buy_order['order_amount']
-            order_size = order_amount / order_price * (1 - fee)
-            # 计算余额
-            info['balance_amount'] = info['balance_amount'] - order_amount
-            info['balance_size'] = info['balance_size'] + order_size
-            info['last_buy_price'] = order_price
-            info['last_buy_amount'] = order_amount
-            info['buy_order'] = None
+    if buy_order is not None and 'submit' == buy_order['state'] \
+            and (low <= buy_order['order_price'] or 'deal_now' == buy_order['flag']):
+        order_price = buy_order['order_price']
+        order_amount = buy_order['order_amount']
+        order_size = order_amount / order_price * (1 - fee)
+        # 计算余额
+        info['balance_amount'] = info['balance_amount'] - order_amount
+        info['balance_size'] = info['balance_size'] + order_size
+        info['last_buy_price'] = order_price
+        info['last_buy_amount'] = order_amount
+        info['buy_order']['state'] = 'finish'
     # 卖单成交，爆单简化，直接成交
-    if sell_order is not None or info['order_num'] >= max_order_level:
-        if sell_order['order_price'] <= high:
-            order_price = sell_order['order_price']
-            order_size = sell_order['order_size']
-            order_amount = order_price * order_size * (1 - fee)
-            # 计算余额
-            info['balance_amount'] = info['balance_amount'] + order_amount
-            info['balance_size'] = info['balance_size'] - order_size
-            info['sell_order'] = None
+    elif sell_order is not None and 'submit' == sell_order['state'] \
+            and (sell_order['order_price'] <= high or 'deal_now' == sell_order['flag']):
+        order_price = sell_order['order_price']
+        order_size = sell_order['order_size']
+        order_amount = order_price * order_size * (1 - fee)
+        # 计算余额
+        info['balance_amount'] = info['balance_amount'] + order_amount
+        info['balance_size'] = info['balance_size'] - order_size
+        info['sell_order']['state'] = 'finish'
 
     # 计算信号，下单
     """
@@ -186,21 +195,48 @@ for candle_begin_time, row in source_df.iterrows():
     4 同时开获利单、下一单 -> 等待成交
     5 获利单成交 -> 取消下一单，本轮结束
     6 下一单成交 -> 取消当前获利单。同时开获利单、下一单
+
+    if buy_order is None:
+        下一单 buy_order
+    elif 'finish' == buy_order.state:
+        sell_order = None
+        下一单 sell_order
+        if order_num < max_order_level:
+            order_num = order_num + 1
+            buy_order = None
+    if 'finish' == sell_order.state:
+        order_num = 0
+        buy_order = None
+        sell_order = None
     """
     balance_amount = info['balance_amount']
     order_num = info['order_num']
     buy_order = info['buy_order']
     sell_order = info['sell_order']
+    close = df.iloc[-1]['close']
 
-    # 1 当前没有单子 -> 下一单
-    if 0 == order_num:
-        close = df.iloc[-1]['close']
-        # 计算价差
-        diff_price = (close * max_diff_rate) / max_order_level
-        # 订单单价 低于当前价一个diff
-        order_price = close - diff_price
-        # 每一份金额
-        per_order_amount = balance_amount / total_level
+    # 没有下单，下一单
+    if buy_order is None and order_num < max_order_level:
+        if 0 == order_num:
+            # 计算价差
+            diff_price = (close * max_diff_rate) / max_order_level
+            # 每一份金额
+            per_order_amount = balance_amount / total_level
+            # 订单单价 第一单简化操作，就等于当前价
+            order_price = close
+            flag = 'deal_now'
+
+            info['diff_price'] = diff_price
+            info['per_order_amount'] = per_order_amount
+        else:
+            # 获取价差
+            diff_price = info['diff_price']
+            # 获取每一份金额
+            per_order_amount = info['per_order_amount']
+            # 订单单价 低于前面价一个diff
+            order_price = info['last_buy_price'] - diff_price
+            flag = ''
+
         # 订单金额 每一份的金额 * 份数
         order_amount = per_order_amount * level_list[order_num]
         # 下一单
@@ -208,96 +244,44 @@ for candle_begin_time, row in source_df.iterrows():
             'order_size': None,                     # 下单卖出的数量，卖单需要
             'order_price': order_price,             # 下单价格
             'order_amount': order_amount,           # 下单买入的金额，买单需要
-            'state': 1                              # 状态 1 下单 2 成交
+            'state': 'submit',                      # 状态 submit 下单 finish 成交
+            'flag': flag,                           # 回测特殊标记，deal_now 第一个买单和最后一个爆单，直接成交
         }
-        info['diff_price'] = diff_price
-        info['per_order_amount'] = per_order_amount
-        info['order_num'] = 1
-    elif 1 == order_num:
-        # 3 一单成交 -> 同时开获利单、下一单
-        if buy_order is None:
-            per_order_amount = info['per_order_amount']
-            close = df.iloc[-1]['close']
-            # 计算价差
-            diff_price = info['diff_price']
-            # 订单单价 低于前面价一个diff
-            order_price = info['last_buy_price'] - diff_price
-            order_amount = per_order_amount * level_list[order_num]
-            info['buy_order'] = {
-                'order_size': None,                     # 下单卖出的数量，卖单需要
-                'order_price': order_price,             # 下单价格
-                'order_amount': order_amount,           # 下单买入的金额，买单需要
-                'state': 1                              # 状态 1 下单 2 成交
-            }
+    if buy_order is not None and 'finish' == buy_order['state']:
+        order_size = info['balance_size']
+        diff_price = info['diff_price']
+        order_price = info['last_buy_price'] + diff_price
+        info['sell_order'] = {
+            'order_size': order_size,     # 下单卖出的数量，卖单需要
+            'order_price': order_price,   # 下单价格
+            'order_amount': None,         # 下单买入的金额，买单需要
+            'state': 'submit',            # 状态 submit 下单 finish 成交
+            'flag': '',
+        }
 
-            balance_size = info['balance_size']
-            order_price = info['last_buy_price'] + diff_price
-            info['sell_order'] = {
-                'order_size': balance_size,  # 下单卖出的数量，卖单需要
-                'order_price': order_price,  # 下单价格
-                'order_amount':  None,  # 下单买入的金额，买单需要
-                'state': 1  # 状态 1 下单 2 成交
-            }
-            info['order_num'] = 2
-        # 2 开一单 -> 等待成交
-        else:
-            pass
-    else:
-        # 4 同时开获利单、下一单 -> 等待成交
-        if buy_order is not None and sell_order is not None:
-            pass
-        # 5 获利单成交 -> 取消下一单，本轮结束
-        elif buy_order is not None and sell_order is None:
-            info['buy_order'] = None
-            info['sell_order'] = None
-            info['order_num'] = 0
-            info['diff_price'] = None
-            info['last_buy_price'] = None
-            info['last_buy_amount'] = None
-        # 6 下一单成交 -> 取消当前获利单。同时开新获利单、下一单
-        elif buy_order is None and sell_order is not None:
-            info['order_num'] = order_num + 1
-            per_order_amount = info['per_order_amount']
-            close = df.iloc[-1]['close']
-            # 计算价差
-            diff_price = info['diff_price']
-            # 订单单价 低于前面价一个diff
-            order_price = info['last_buy_price'] - diff_price
-            if order_num >= max_order_level:
-                # 爆了，卖掉所有单子
-                balance_size = info['balance_size']
-                order_price = info['last_buy_price']
-                info['sell_order'] = {
-                    'order_size': balance_size,  # 下单卖出的数量，卖单需要
-                    'order_price': order_price,  # 下单价格
-                    'order_amount': None,  # 下单买入的金额，买单需要
-                    'state': 1  # 状态 1 下单 2 成交
-                }
-                continue
-
-            order_amount = per_order_amount * level_list[order_num]
-            info['buy_order'] = {
-                'order_size': None,  # 下单卖出的数量，卖单需要
-                'order_price': order_price,  # 下单价格
-                'order_amount': order_amount,  # 下单买入的金额，买单需要
-                'state': 1  # 状态 1 下单 2 成交
-            }
-
-            balance_size = info['balance_size']
-            order_price = info['last_buy_price'] + diff_price
-            info['sell_order'] = {
-                'order_size': balance_size,  # 下单卖出的数量，卖单需要
-                'order_price': order_price,  # 下单价格
-                'order_amount': None,  # 下单买入的金额，买单需要
-                'state': 1  # 状态 1 下单 2 成交
-            }
-        # 7 理论不可能发生，获利单、下一单都成交 -> 本轮结束
-        else:
-            info['buy_order'] = None
-            info['sell_order'] = None
-            info['order_num'] = 0
-            info['diff_price'] = None
-            info['last_buy_price'] = None
-            info['last_buy_amount'] = None
+        info['order_num'] = order_num + 1
+        info['buy_order'] = None
+    if sell_order is not None and 'finish' == sell_order['state']:
+        info['order_num'] = 0
+        info['buy_order'] = None
+        info['sell_order'] = None
+    # 最后一个num要考虑破网，如果下跌超过一个diff，重新提交卖单，直接成交
+    if order_num == max_order_level \
+            and sell_order is not None \
+            and (info['last_buy_price'] - close) > info['diff_price']:
+        order_size = info['balance_size']
+        order_price = close
+        info['sell_order'] = {
+            'order_size': order_size,  # 下单卖出的数量，卖单需要
+            'order_price': order_price,  # 下单价格
+            'order_amount': None,  # 下单买入的金额，买单需要
+            'state': 'submit',  # 状态 submit 下单 finish 成交
+            'flag': 'deal_now',
+        }
 
 exit()
+
+# =====并行提速
+with Pool(processes=8) as pool:  # or whatever your hardware can support
+    # 使用并行批量获得data frame的一个列表
+    pool.map(calculate_by_one_loop, para_list)
