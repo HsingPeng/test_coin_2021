a28593082e472fb0c19ef011baef9525a34f11f4
HsingPeng <bboxhe@gmail.com>
2021-05-16 20:09:23 +0800

    add okex swap rate monitor

---
+++ swap_rate_fee/swap_rate_monitor_okex.py
@@ -0,0 +1,162 @@
+"""
+okex监控永续资金费率，每天8：05运行，发送信息包含 当日，7日年化，30日年化，
+本程序按utc时间撰写
+更新时间：20200926
+"""
+import ccxt
+import pandas as pd
+import math
+from time import sleep
+import hmac
+import hashlib
+import base64
+import json
+import requests
+import time
+import traceback
+from urllib import parse
+from datetime import datetime, timedelta
+import conf
+
+pd.set_option('expand_frame_repr', False)
+# pd.set_option('display.max_rows', 500)
+# pd.set_option('display.min_rows', 500)
+
+# ===创建交易所实例（使用公共接口，不需要apikey）
+okex = ccxt.okex()
+
+wx_webhook = conf.Config.wx_webhook
+# 针对网络设置代理
+if conf.Config.proxies is not None:
+    okex.proxies = conf.Config.proxies
+
+
+def send_weixin_msg(content, webhook):
+    try:
+        headers = {"Content-Type": "application/json;charset=utf-8"}
+        msg = {
+            "msgtype": "markdown",
+            "markdown": {
+                "content": content,
+            }
+        }
+        body = json.dumps(msg)
+        requests.post(webhook, data=body, headers=headers)
+        print('成功发送钉钉')
+    except Exception as e:
+        print("发送钉钉失败:", e)
+
+
+# ===定时运行
+def next_run_time():
+    now = datetime.utcnow()
+    now = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    target_time = now + timedelta(days=1)
+    target_time += timedelta(minutes=5)
+
+    return target_time
+
+# ===获取单个币种的资金费率
+# 获取资金费率
+def get_swap_funding_rate(instrument_id, max_try_num=5):
+    param = {'instrument_id': instrument_id, 'limit': 100}  # limit最大为100，获取过去30天的历史资金费率
+    for i in range(max_try_num):
+        try:
+            rate = okex.swap_get_instruments_instrument_id_historical_funding_rate(param)
+            return pd.DataFrame(rate, dtype=float)
+        except Exception as e:
+            print(f'获取{instrument_id}资金费率错误，1s后重试:{e}')
+            sleep(1)
+
+
+# 单币种数据处理
+def funding_rate_analyse(instrument_id):
+
+    _df = get_swap_funding_rate(instrument_id)
+
+    # ==整体时间并转化为1天的周期
+    _df['funding_time'] = pd.to_datetime(_df['funding_time'])  #
+    _df = _df.resample(rule='1D', on='funding_time', label='left', closed='left').agg(
+        {
+            'instrument_id': 'first',
+            'funding_rate': 'sum',  # 稍微有些差异，本应该是复利连乘，这里采为sum，近似相等
+            'realized_rate': 'sum',
+         })
+
+    # 重置index
+    _df.reset_index(inplace=True)
+
+    # 剔除最后一条数据。utc 0点运行，当天的只产生一条数据，求年化失真
+    _df = _df[:-1]
+    # ===计算统计量（当日年化， 7日年化，30日年化 %）    日复利
+    # 当日年化
+    _df['当日年化'] = round((pow((1 + _df['realized_rate']), 365) - 1) * 100, 2)
+    # 7日年化
+    _df['rtn_7d'] = _df['realized_rate'].rolling(7).apply(lambda x: (1 + x).prod())
+    _df['7日年化'] = round((pow(_df['rtn_7d'], 365 / 7) - 1) * 100, 2)
+    # 30日年化
+    _df['rtn_30d'] = _df['realized_rate'].rolling(30).apply(lambda x: (1 + x).prod())
+    _df['30日年化'] = round((pow(_df['rtn_30d'], 365 / 30) - 1) * 100, 2)
+
+    # 保留数据
+    _df = _df[['funding_time', 'instrument_id', 'realized_rate', '30日年化', '7日年化', '当日年化']]
+
+    return _df
+
+
+# ===主函数
+def main():
+    # ===查找所有币本位永续的instrument_id
+    instrument_id_list = okex.swap_get_instruments()
+    instrument_id_list = pd.DataFrame(instrument_id_list)
+    instrument_id_list = list(instrument_id_list[instrument_id_list['quote_currency'].isin(['USD', 'USDT'])]['instrument_id'])
+    # instrument_id_list = ['ETH-USD-SWAP', 'BTC-USD-SWAP']
+    # ===获取永续的资金费率，合并
+    all_coin_rate = pd.DataFrame()
+    # 循环计算数据
+    for instrument_id in instrument_id_list:
+        df = funding_rate_analyse(instrument_id)
+        all_coin_rate = all_coin_rate.append(df.iloc[-1], ignore_index=True)
+
+    # 整理结果
+    all_coin_rate = all_coin_rate[['instrument_id', '30日年化', '7日年化', '当日年化']]
+    all_coin_rate.sort_values(by=['30日年化', '7日年化', '当日年化'], ascending=[0, 0, 0], inplace=True)
+    all_coin_rate.set_index('instrument_id', drop=True, inplace=True)
+
+    # 发送通知
+    content = '## **OKEX永续币本位资金费率** \n'
+    content += '  **| 30日年化% | 7日年化% | 当日年化% |** \n'
+    for x, y in all_coin_rate.iterrows():
+        x = x.replace('-SWAP', '')
+        content += '\n**' + str(x) + '**：'
+        y = list(y)
+        for _ in y:
+            content += str(_) + '  |  '
+        content += ' \n'
+    content += ' \n ' + datetime.now().strftime("%m-%d %H:%M:%S")
+    print(content)
+    send_weixin_msg(content, wx_webhook)
+
+    # ===定时自动运行
+    target_time = next_run_time()  # 北京时间每天早08：05, 即utc时间00：05
+    print('本次运行完成，下次运行时间(UTC)：', target_time)
+    sleep(max(0, (target_time - datetime.utcnow()).seconds))
+
+
+# 主程序
+if __name__ == '__main__':
+    i = 0
+    while True:
+        try:
+            main()
+            i = 0
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+            print(f'okex资金费率监控，第{i}次尝试，运行报错{e}')
+            send_weixin_msg(f'okex资金费率监控，第{i}次尝试，运行报错{e}', wx_webhook)
+            sleep(10)
+            if i >= 5:
+                print('程序重试次数过多，退出')
+                send_weixin_msg(f'资金费率，程序重试次数过多，已退出', wx_webhook)
+                exit()
--- trade_frame/strategy/spot_neutral_1.py
+++ trade_frame/strategy/spot_neutral_1.py
@@ -32,7 +32,7 @@ class SpotNeutral1:
         exchange = _controller.get_exchange()
         logger = exchange.logger
 
-        diff_rate, target_coin, base_coin, per_usdt = params.split('-')
+        diff_rate, target_coin, base_coin, total_coin_value = params.split('-')
 
         # 初始化
         # diff_rate = "0dot0009"
@@ -41,7 +41,12 @@ class SpotNeutral1:
         # per_usdt = 11  # 由于当前比较特殊，资金不够。所以每次交易都用11usdt。之后改成比例
 
         diff_rate = float(diff_rate)
-        per_usdt = float(per_usdt)
+        total_coin_value = float(total_coin_value)
+        per_usdt = total_coin_value * diff_rate
+        min_usdt = 10
+        if per_usdt < min_usdt:
+            raise Exception('由于 binance 限制，每次开单必须要大于%su' % min_usdt)
+
         symbol = target_coin + '/' + base_coin
 
         sleep_time = 0.1        # 睡眠时间
@@ -56,25 +61,56 @@ class SpotNeutral1:
         else:
             extra_fee_mode = False
 
-        log_startone_header = ['realtime', 'std_price', 'target_coin', 'base_coin', 'total_value', 'fee_usdt',
+        log_startone_header = ['realtime', 'std_price', 'target_coin', 'base_coin', 'fee_usdt',
                                'init_value', 'current_value', 'profit_rate', 'max_drawdown', 'win_num', 'lose_num',
-                               'win_rate']
+                               'win_rate', 'profit_rate_nofee', 'current_coin_value']
         _controller.header_to_csv(log_startone_header, 'startone')
+
+        # 第一次平衡资金
+        ticker_info = exchange.fetch_ticker(symbol)
+        std_price = ticker_info['last']  # 基准价格，每轮重置
+        balance_info = exchange.fetch_balance()
+
+        balance_order_info = None
+        default_coin_value = balance_info[target_coin]['total'] * std_price
+        if total_coin_value - default_coin_value > min_usdt:     # 少了买一点
+            balance_amount = total_coin_value - default_coin_value
+            balance_order_info = exchange.create_market_buy_order(symbol, balance_amount)
+            logger.info('[balance value]buy target coin=%s cost=%s' % (target_coin, balance_amount))
+        elif default_coin_value - total_coin_value > min_usdt:   # 多了卖一点
+            balance_amount = (default_coin_value - total_coin_value) / std_price
+            balance_order_info = exchange.create_market_sell_order(symbol, balance_amount)
+            logger.info('[balance value]sell target coin=%s amount=%s' % (target_coin, balance_amount))
+
+        if balance_order_info is not None:
+            # 循环等待完全成交
+            not_finish = True
+            while not_finish:
+                # 如果没有完成，休眠
+                exchange.sleep(sleep_time * 2)
+                orders_info = exchange.fetch_orders(symbol)
+                for one_order in orders_info:
+                    if balance_order_info['id'] == one_order['id'] and 'closed' == one_order['status']:
+                        if extra_fee_mode:
+                            exchange.add_fee_usdt(one_order['cost'])
+                        not_finish = False
 
         # 开始循环
         while True:
             # 每轮都重新获取当前价格
             ticker_info = exchange.fetch_ticker(symbol)
             std_price = ticker_info['last']     # 基准价格，每轮重置
-            min_price = std_price            # 判断回调使用，每轮重置
+            min_price = ticker_info['last']            # 判断回调使用，每轮重置
 
             balance_info = exchange.fetch_balance()
             if init_value is None:
                 init_price = std_price
                 init_value = balance_info[target_coin]['total'] * init_price + balance_info[base_coin]['total']
+                default_coin_value = balance_info[target_coin]['total'] * init_price
 
-            current_value = balance_info[target_coin]['total'] * init_price + balance_info[base_coin]['total'] \
+            current_value = balance_info[target_coin]['total'] * std_price + balance_info[base_coin]['total'] \
                 - exchange.get_fee_usdt()
+            current_coin_value = balance_info[target_coin]['total'] * std_price
             if min_value is None:
                 min_value = current_value
             else:
@@ -85,19 +121,14 @@ class SpotNeutral1:
                 max_value = max(max_value, current_value)
 
             # 平衡资金
-            if current_value < per_usdt * 2:
-                raise Exception('balance is not enough, something wrong happened')
-
-            balance_order_info = None
-            if balance_info[base_coin]['total'] > per_usdt * 400:  # base coin 多于400次，立刻买入，只留200次，因为要尽量持有
-                balance_amount = balance_info[base_coin]['total'] - per_usdt * 200
+            if total_coin_value - current_coin_value > min_usdt:  # 少了买一点
+                balance_amount = total_coin_value - current_coin_value
                 balance_order_info = exchange.create_market_buy_order(symbol, balance_amount)
-                logger.info('[balance value]buy target coin=%s cost=%s' % (target_coin, balance_amount))
-            elif balance_info[base_coin]['total'] < per_usdt * 2:      # base coin 不足，卖一些 target coin，得到50次
-                balance_amount = per_usdt / std_price * 100
+                logger.info('[balance value]buy target coin=%s cost=%s current_value=%s' % (target_coin, balance_amount, current_value))
+            elif current_coin_value - total_coin_value > min_usdt:  # 多了卖一点
+                balance_amount = (current_coin_value - total_coin_value) / std_price
                 balance_order_info = exchange.create_market_sell_order(symbol, balance_amount)
-                logger.info('[balance value]sell target coin=%s amount=%s' % (target_coin, balance_amount))
-
+                logger.info('[balance value]sell target coin=%s amount=%s current_value=%s' % (target_coin, balance_amount, current_value))
             if balance_order_info is not None:
                 # 循环等待完全成交
                 not_finish = True
@@ -110,20 +141,19 @@ class SpotNeutral1:
                             if extra_fee_mode:
                                 exchange.add_fee_usdt(one_order['cost'])
                             not_finish = False
-
-                continue    # 重开一轮
+                continue
 
             log_startone = {
                 'realtime': exchange.get_str_time(),
                 'std_price': std_price,
                 'target_coin': balance_info[target_coin]['total'],
                 'base_coin': balance_info[base_coin]['total'],
-                'total_value': (balance_info[base_coin]['total']
-                                + std_price * balance_info[target_coin]['total'] - exchange.get_fee_usdt()),
+                'current_coin_value': current_coin_value,
                 'fee_usdt': exchange.get_fee_usdt(),
                 'init_value': init_value,
                 'current_value': current_value,
                 'profit_rate': ((current_value - init_value) / init_value),
+                'profit_rate_nofee': ((current_value - init_value + exchange.get_fee_usdt()) / init_value),
                 'max_drawdown': ((max_value - min_value) / max_value),
                 'win_num': win_num,
                 'lose_num': lose_num,
@@ -136,18 +166,25 @@ class SpotNeutral1:
             logger.info("[start one]%s" % "\t".join(logline))
 
             # 开一单，卖单
-            price = std_price * (1 + diff_rate)
-            amount = per_usdt / price
-            sell_order_info = exchange.create_limit_sell_order(symbol, amount, price)
-            logger.debug('[%s] [create sell]amount=%s, price=%s' %
-                          (exchange.get_str_time(), sell_order_info['amount'], sell_order_info['price']))
+            pos_price = std_price * (1 + diff_rate)
+            pos_value_diff = balance_info[target_coin]['total'] * pos_price - default_coin_value
+            sell_order_info = None
 
             # 开一单，买单
-            price = std_price * (1 - diff_rate)
-            amount = per_usdt / price
-            buy_order_info = exchange.create_limit_buy_order(symbol, amount, price)
-            logger.debug('[%s] [create buy]amount=%s, price=%s' %
-                          (exchange.get_str_time(), buy_order_info['amount'], buy_order_info['price']))
+            negative_price = std_price * (1 - diff_rate)
+            negative_value_diff = default_coin_value - balance_info[target_coin]['total'] * negative_price
+            buy_order_info = None
+            print(111, pos_value_diff, negative_value_diff)
+            if pos_value_diff > min_usdt:       # 卖掉多的币
+                amount = (balance_info[target_coin]['total'] * pos_price - default_coin_value) / pos_price
+                sell_order_info = exchange.create_limit_sell_order(symbol, amount, pos_price)
+                logger.debug('[%s] [create sell]amount=%s, price=%s' %
+                             (exchange.get_str_time(), sell_order_info['amount'], sell_order_info['price']))
+            if negative_value_diff > min_usdt:  # 买
+                amount = (default_coin_value - balance_info[target_coin]['total'] * negative_price) / negative_price
+                buy_order_info = exchange.create_limit_buy_order(symbol, amount, negative_price)
+                logger.debug('[%s] [create buy]amount=%s, price=%s' %
+                             (exchange.get_str_time(), buy_order_info['amount'], buy_order_info['price']))
 
             # 循环等待完全成交
             not_finish = True
@@ -194,7 +231,7 @@ class SpotNeutral1:
 
             balance_info = exchange.fetch_balance()
 
-            current_value = balance_info[target_coin]['total'] * init_price + balance_info[base_coin]['total'] \
+            current_value = balance_info[target_coin]['total'] * std_price + balance_info[base_coin]['total'] \
                 - exchange.get_fee_usdt()
             min_value = min(min_value, current_value)
             max_value = max(max_value, current_value)
@@ -204,12 +241,12 @@ class SpotNeutral1:
                     'std_price=%f' % std_price,
                     'target_coin=%f' % balance_info[target_coin]['total'],
                     'base_coin=%f' % balance_info[base_coin]['total'],
-                    'total_value=%f' % (balance_info[base_coin]['total']
-                                        + std_price * balance_info[target_coin]['total'] - exchange.get_fee_usdt()),
+                    'target_coin_value=%f' % (std_price * balance_info[target_coin]['total']),
                     'fee_usdt=%f' % exchange.get_fee_usdt(),
                     'init_value=%f' % init_value,
                     'current_value=%f' % current_value,
                     'profit_rate=%f' % ((current_value - init_value) / init_value),
+                    'profit_rate_nofee=%f' % ((current_value - init_value + exchange.get_fee_usdt()) / init_value),
                     'max_drawdown=%f' % ((max_value - min_value) / max_value),
                 ]
             ))
