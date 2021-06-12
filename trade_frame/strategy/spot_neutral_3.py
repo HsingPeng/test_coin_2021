c9e21a78495aede71e791dd6653a9c4874e3c5c6
HsingPeng <bboxhe@gmail.com>
2021-06-12 20:18:01 +0800

    fix spot_neutral_3 error

--- trade_frame/strategy/spot_neutral_3.py
+++ trade_frame/strategy/spot_neutral_3.py
@@ -7,12 +7,12 @@ diff_price = std_price * diff_rate
 
 下跌 N * diff_price 再回弹一个 diff_price，则结束。
 只记录
-- 上涨为 num=0
-- 下跌一个为 num=1
-- 下跌两个及以上为 num=2+
+- 上涨为 num=-1
+- 下跌一个为 num=0
+- 下跌两个及以上为 num=1+
 
 统计：
--  "1" / ("1" + "2+") - 1
+-  "0" / ("0" + "1+") - 1
 - 1天比例 3天比例 5天比例
 
 """
@@ -37,31 +37,32 @@ class SpotNeutral3:
         df = pandas.DataFrame()
         series = pandas.Series({
             'time': exchange.get_int_time(),
-            '0': 0,
-            '1': 1,
-            '2+': 1,
+            '-1': 0,
+            '0': 1,
+            '1+': 1,
             'total': 2,
         }, name=exchange.get_int_time())
         df = df.append(series, ignore_index=True)
         data_all = {
-            '0': 0,
-            '1': 1,
-            '2+': 1,
+            '-1': 0,
+            '0': 1,
+            '1+': 1,
             'total': 2,
         }
 
         log_startone_header = [
             'realtime',
             'std_price',
+            'num=-1',
             'num=0',
-            'num=1',
-            'num=2+',
+            'num=1+',
             'num_total',
             'win_rate',
             'win_rate_5d',
             'win_rate_3d',
             'win_rate_1d',
             'win_rate_12h',
+            'win_rate_6h',
             ]
         _controller.header_to_csv(log_startone_header, 'startone')
 
@@ -84,22 +85,22 @@ class SpotNeutral3:
                                  (exchange.get_str_time(), current_price, std_price))
                     data_now = {
                         'time': exchange.get_int_time(),
+                        '-1': 0,
                         '0': 0,
-                        '1': 0,
-                        '2+': 0,
+                        '1+': 0,
                         'total': 1,
                     }
                     data_all['total'] += 1
 
                     if N < 0:
+                        data_now['-1'] = 1
+                        data_all['-1'] += 1
+                    elif N == 0:
                         data_now['0'] = 1
                         data_all['0'] += 1
-                    elif N == 1:
-                        data_now['1'] = 1
-                        data_all['1'] += 1
                     else:
-                        data_now['2+'] = 1
-                        data_all['2+'] += 1
+                        data_now['1+'] = 1
+                        data_all['1+'] += 1
                     series = pandas.Series(data_now, name=exchange.get_int_time())
                     df = df.append(series, ignore_index=True)
                     break
@@ -108,18 +109,20 @@ class SpotNeutral3:
             df_3d = df[df['time'] > (exchange.get_int_time() - 3600 * 24 * 3)]     # 3天
             df_1d = df[df['time'] > (exchange.get_int_time() - 3600 * 24 * 1)]     # 1天
             df_12h = df[df['time'] > (exchange.get_int_time() - 3600 * 12)]     # 12小时
+            df_6h = df[df['time'] > (exchange.get_int_time() - 3600 * 6)]     # 12小时
             log_startone = {
                 'realtime': exchange.get_str_time(),
                 'std_price': std_price,
+                'num=-1': data_all['-1'],
                 'num=0': data_all['0'],
-                'num=1': data_all['1'],
-                'num=2+': data_all['2+'],
+                'num=1+': data_all['1+'],
                 'num_total': data_all['total'],
-                'win_rate': data_all['1'] / (data_all['1'] + data_all['2+']) - 0.5,
-                'win_rate_5d': (df['1'].sum() / (df['1'].sum() + df['2+'].sum())) - 0.5,
-                'win_rate_3d': (df_3d['1'].sum() / (df_3d['1'].sum() + df_3d['2+'].sum())) - 0.5,
-                'win_rate_1d': (df_1d['1'].sum() / (df_1d['1'].sum() + df_1d['2+'].sum())) - 0.5,
-                'win_rate_12h': (df_12h['1'].sum() / (df_12h['1'].sum() + df_12h['2+'].sum())) - 0.5,
+                'win_rate': data_all['0'] / (data_all['0'] + data_all['1+']),
+                'win_rate_5d': (df['0'].sum() / (df['0'].sum() + df['1+'].sum())),
+                'win_rate_3d': (df_3d['0'].sum() / (df_3d['0'].sum() + df_3d['1+'].sum())),
+                'win_rate_1d': (df_1d['0'].sum() / (df_1d['0'].sum() + df_1d['1+'].sum())),
+                'win_rate_12h': (df_12h['0'].sum() / (df_12h['0'].sum() + df_12h['1+'].sum())),
+                'win_rate_6h': (df_6h['0'].sum() / (df_6h['0'].sum() + df_6h['1+'].sum())),
             }
             _controller.data_to_csv(log_startone_header, [log_startone], 'startone')
 
