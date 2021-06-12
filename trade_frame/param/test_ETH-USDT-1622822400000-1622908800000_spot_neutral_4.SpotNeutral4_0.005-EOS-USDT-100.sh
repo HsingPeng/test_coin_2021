8d93b623c2cf8786ca167022f1cd70e2aae27436
HsingPeng <bboxhe@gmail.com>
2021-06-12 23:31:43 +0800

    add spot_neutral_4

---
+++ trade_frame/param/test_ETH-USDT-1622822400000-1622908800000_spot_neutral_4.SpotNeutral4_0.005-EOS-USDT-100.sh
@@ -0,0 +1,2 @@
+python3 backtesting.py test ETH-USDT-1622822400000-1622908800000 spot_neutral_4.SpotNeutral4 0.005-ETH-USDT-100
+python3 show_csv_plot.py output/backtesting_test_ETH-USDT-1622822400000-1622908800000_spot_neutral_4.SpotNeutral4_0.005-ETH-USDT-100_startone.csv realtime
